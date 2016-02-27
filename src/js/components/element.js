03d3e052cefd9b821990306dce22cd09bfeb053d
nickroberts404 <nickroberts404@gmail.com>
2016-02-27 17:14:26 -0600

    Trying to fix subtraction on touch devices

--- src/js/components/element.js
+++ src/js/components/element.js
@@ -8,6 +8,9 @@ class Element extends React.Component {
 		super();
 		this.onClick = this.onClick.bind(this);
 		this.onRightClick = this.onRightClick.bind(this);
+		this.onTouchStart = this.onTouchStart.bind(this);
+		this.onTouchEnd = this.onTouchEnd.bind(this);
+		this.state = { addNew: true, timeout: null }
 	}
 	onClick () {
 		this.props.onClick(this.props.element);
@@ -15,6 +18,20 @@ class Element extends React.Component {
 	onRightClick (e) {
 		e.preventDefault();
 		this.props.onRightClick(this.props.element);
+	}
+	onTouchStart () {
+		var timer = window.setTimeout(() => {
+			this.props.onRightClick(this.props.element);
+			this.setState({addNew: false})
+		}, 500 )
+		this.setState({timeout: timer});
+	}
+	onTouchEnd () {
+		window.clearTimeout(this.state.timeout)
+		if (this.state.addNew) {
+			this.props.onClick(this.props.element);
+			this.setState({addNew: false })
+		}
 	}
 	render () {
 		var data = PeriodicTable.getElement(this.props.element);
@@ -25,6 +42,8 @@ class Element extends React.Component {
 			<td 
 				className = {extraClass+'element type-'+data.type}
 				onClick = {this.onClick}
+				onTouchStart = {this.onTouchStart}
+				onTouchEnd = {this.onTouchEnd}
 				onContextMenu = {this.onRightClick}>
 				<div className = 'content'>
 					<div className="number">{data.number}</div>
